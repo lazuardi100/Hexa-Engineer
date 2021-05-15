373cc727f27f43ae9f249eef9b0a6bd2415f5667
lazuardi100 <lazuardi.akhmad@gmail.com>
2021-05-15 17:48:14 +0700

    add connection to mysql

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = '34.101.238.80'
+app.config['MYSQL_USER'] = 'user'
+app.config['MYSQL_PASSWORD'] = 'Rahasia'
+app.config['MYSQL_DB'] = 'BANGKIT'
+mysql = MySQL(app)
 
 @app.route("/")
 def index():
