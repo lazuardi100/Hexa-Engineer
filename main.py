5e67feb7a4088d6a771a678ad4d9a1a9cd593d64
lazuardi100 <lazuardi.akhmad@gmail.com>
2021-05-15 18:00:44 +0700

    testing adding user

--- main.py
+++ main.py
@@ -7,8 +7,20 @@ app.config['MYSQL_HOST'] = '34.101.238.80'
 app.config['MYSQL_USER'] = 'user'
 app.config['MYSQL_PASSWORD'] = 'Rahasia'
 app.config['MYSQL_DB'] = 'BANGKIT'
+
 mysql = MySQL(app)
 
 @app.route("/")
 def index():
     return "Hello world"
+
+@app.route("/adduser")
+def adduser():
+    cur = mysql.connection.cursor()
+    cur.execute('INSERT INTO USER(NAMA, PANGGILAN) VALUES (\'AKHMAD\', \'LAZUARDI\')')
+    mysql.connection.commit()
+    cur.close()
+    return 'sukses'
+
+if __name__ == '__main__':
+    app.run()
